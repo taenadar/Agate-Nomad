da033d573d3a5a409b3073b6502bf2f7f2c198d8
Vincent Karsten <vkarsten@mirabeau.nl>
2013-11-29 15:38:42 +0100

    created dynamic routes to catch all urls

--- AgateNomad/Global.asax.cs
+++ AgateNomad/Global.asax.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -17,7 +18,51 @@ namespace AgateNomad
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ControllerBuilder.Current.SetControllerFactory(new ControllerFactory());
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+    }
+
+    public class ControllerFactory : DefaultControllerFactory
+    {
+        public override IController CreateController(System.Web.Routing.RequestContext requestContext, string controllerName)
+        {
+            if (requestContext == null)
+            {
+                throw new ArgumentNullException("requestContext");
+            }
+
+            if (String.IsNullOrEmpty(controllerName))
+            {
+                throw new ArgumentException("MissingControllerName");
+            }
+
+            var controllerType = GetControllerType(requestContext, controllerName);
+
+            // This is where a 404 is normally returned
+            // Replaced with route to catchall controller
+            if (controllerType == null)
+            {
+                // Build the dynamic route variable with all segments
+                var dynamicRoute = "";
+                foreach (var segment in requestContext.RouteData.Values.Values)
+                {
+                    dynamicRoute += segment + "/";
+                }
+                // Remove the last '/'
+                dynamicRoute = dynamicRoute.Substring(0, dynamicRoute.Length - 1);
+
+                // Route to the Catchall controller
+                controllerName = "CatchAll";
+                controllerType = GetControllerType(requestContext, controllerName);
+                requestContext.RouteData.Values["Controller"] = controllerName;
+                requestContext.RouteData.Values["action"] = "Index";
+                requestContext.RouteData.Values["dynamicRoute"] = dynamicRoute;
+            }
+
+            IController controller = GetControllerInstance(requestContext, controllerType);
+            return controller;
+        }
+
     }
 }
